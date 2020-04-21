6bfd2dad0beb41d47f5674016bb5a47d7131e6e0
random <lrandoml@protonmail.com>
2020-04-21 22:16:28 +0200

    (:

--- test1.py
+++ test1.py
@@ -13,48 +13,65 @@ for i in word:
 print(_help)
 print("U have 5 tries.")
 
-tries = 5
-correct = 0
-letter = []
-num = ["1","2","3","4","5","6","7","9","0"]
 
-while True:
-    if correct == len(word):
-        print("got it")
-        break
-    
-    if tries == 0:
-        print("game over")
-        break
-    
-    print("letter or word?")
-    _try = str(input(">"))
 
-    if _try in letter:
-        print("given")
-        tries -=1
-        print(f"{tries} tries remaining")
+def test():
     
-    elif _try in num:
-        print("just letter or word.")
+    fdg = 0
+    tries = 5
+    correct = 0
+    letter = []
+    num = ["1","2","3","4","5","6","7","9","0"]
+    
+    while True:
+        if correct == len(word):
+            print("got it")
+            break
+        
+        if tries == 0:
+            print("game over")
+            break
+        
+        print("letter or word?")
+        _try = str(input(">"))
+    
+        if _try == "" and fdg > 0:
+            tries -=1
+            print(f"{tries} tries remaining")
 
-    elif len(_try) > 1 and _try != word:
-        print("one letter or the word.")
-    
-    elif _try == word:
-        print("got it")
-        break
-    
-    elif _try in word:
-        letter.append(_try)
-        print(_try,"is correct")
-        correct += 1
-        for index,x in enumerate(word):
-            if _try == x:
-                _help[index] = _try
-        print(_help)
-    
-    else:
-        tries -= 1
-        print("false")
-        print(f"{tries} tries remaining")
+        if _try in letter:
+            print("given")
+            tries -=1
+            print(f"{tries} tries remaining")
+        
+        
+        elif _try in num:
+            print("just letter or word.")
+
+        elif len(_try) > 1 and _try != word:
+            print("nop")
+            tries -= 1
+            print(f"{tries} tries remaining")
+            
+        elif _try == word:
+            print("got it")
+            break
+
+        elif _try == "":
+            print("need charactere")
+            fdg += 1
+        
+        elif _try in word:
+            letter.append(_try)
+            print(_try,"is correct")
+            correct += 1
+            for index,x in enumerate(word):
+                if _try == x:
+                    _help[index] = _try
+            print(_help)
+        
+        else:
+            tries -= 1
+            print("false")
+            print(f"{tries} tries remaining")
+test()
